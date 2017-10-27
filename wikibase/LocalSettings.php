c75ffc7938ced64804a236b88de18eb91cb73695
addshore <addshorewiki@gmail.com>
2017-10-27 14:26:45 +0100

    Install mediawiki

--- wikibase/LocalSettings.php
+++ wikibase/LocalSettings.php
@@ -1,1 +1,18 @@
 <?php
+
+## Database settings
+$wgDBserver = "mysql";
+$wgDBname = "my_wiki";
+$wgDBuser = "wikiuser";
+$wgDBpassword = "sqlpass";
+
+## Locale
+
+$wgShellLocale = "en_US.utf8";
+$wgLanguageCode = "en";
+
+$wgSitename = "docker-wikibase";
+$wgMetaNamespace = "Project";
+$wgScriptPath = "";
+
+wfLoadSkin( 'Vector' );
--- wikibase/entrypoint.sh
+++ wikibase/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Wait for the db to come up
+/wait-for-it.sh mysql:3306
+
 # Do the mediawiki install
+# TODO if LocalSettings already exists DONT run install.php!!
 php /var/www/html/maintenance/install.php --dbuser wikiuser --dbpass sqlpass --dbname my_wiki --dbserver mysql --lang en --pass adminpass docker-wikibase admin
+
+# Copy our LocalSettings into place after install
+cp /LocalSettings.php /var/www/html/
 
 # Run the actual entry point
 docker-php-entrypoint apache2-foreground
