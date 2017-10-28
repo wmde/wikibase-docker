ebe68b45b090246b9b475bfa3ae51516e540988a
addshore <addshorewiki@gmail.com>
2017-10-28 14:23:28 +0200

    Fix container dependancies / order

--- wikibase/entrypoint.sh
+++ wikibase/entrypoint.sh
@@ -2,6 +2,10 @@
 
 # Wait for the db to come up
 /wait-for-it.sh mysql:3306
+# Sometimes it appears to come up and then go back down meaning MW install fails
+# So wait for a second and double check!
+sleep 1
+/wait-for-it.sh mysql:3306
 
 # Do the mediawiki install
 # TODO if LocalSettings already exists DONT run install.php!!
