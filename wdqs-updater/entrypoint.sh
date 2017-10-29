89da224dbf13cbcb02632bb160a50bd9d8bf2937
addshore <addshorewiki@gmail.com>
2017-10-29 14:15:32 +0100

    HACK HACK HACK :( but YAY demo!

--- wdqs-updater/entrypoint.sh
+++ wdqs-updater/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/wait-for-it.sh wikibase:80 -t 60
+/wait-for-it.sh wikibase.svc:80 -t 60
 /wait-for-it.sh wdqs:9999 -t 60
 
-/wdqs/runUpdate.sh -h http://wdqs:9999 -- --wikibaseHost wikibase --wikibaseScheme http --entityNamespaces 120,122
+/wdqs/runUpdate.sh -h http://wdqs:9999 -- --wikibaseHost wikibase.svc --wikibaseScheme http --entityNamespaces 120,122
