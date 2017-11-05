ed066fb52e294dc7b2ded855470fe1642ae84f70
addshore <addshorewiki@gmail.com>
2017-11-05 14:45:06 +0000

    T179303 envsubst for wikibase image

--- wikibase/entrypoint.sh
+++ wikibase/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Wait for the db to come up
-/wait-for-it.sh mysql.svc:3306 -t 60
+/wait-for-it.sh $DB_SERVER -t 60
 # Sometimes it appears to come up and then go back down meaning MW install fails
 # So wait for a second and double check!
 sleep 1
-/wait-for-it.sh mysql.svc:3306 -t 60
+/wait-for-it.sh $DB_SERVER -t 60
 
 # Do the mediawiki install
 # TODO if LocalSettings already exists DONT run install.php!!
-php /var/www/html/maintenance/install.php --dbuser wikiuser --dbpass sqlpass --dbname my_wiki --dbserver mysql.svc --lang en --pass adminpass docker-wikibase admin
+php /var/www/html/maintenance/install.php --dbuser $DB_USER --dbpass $DB_PASS --dbname $DB_NAME --dbserver $DB_SERVER --lang $MW_SITE_LANG --pass $MW_ADMIN_PASS $MW_SITE_NAME $MW_ADMIN_NAME
 
-# Copy our LocalSettings into place after install
-cp /LocalSettings.php /var/www/html/
+# Copy our LocalSettings into place after install from the template
+# https://stackoverflow.com/a/24964089/4746236
+export DOLLAR='$'
+envsubst < /LocalSettings.php.template > /var/www/html/LocalSettings.php
 
 # Run update.php to install Wikibase
 php /var/www/html/maintenance/update.php --quick
