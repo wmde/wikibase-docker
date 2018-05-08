83cfebf1e91dff9adaf5a1e0087f37e98e2400b1
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-05-08 09:56:52 +0100

    travis build scripts for split base bundle

--- wikibase/1.30/.travis/build-deploy.sh
+++ wikibase/1.30/.travis/build-deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build "$DIR/../" -t wikibase/wikibase:1.30 -t wikibase/wikibase:latest
+docker build "$DIR/../base" -t wikibase/wikibase:1.30 -t wikibase/wikibase:1.30-base -t wikibase/wikibase:latest -t wikibase/wikibase:latest-base
+docker build "$DIR/../bundle" -t wikibase/wikibase:1.30-bundle -t wikibase/wikibase:latest-bundle
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
     docker push wikibase/wikibase:1.30
     docker push wikibase/wikibase:latest
+    docker push wikibase/wikibase:1.30-base
+    docker push wikibase/wikibase:latest-base
+    docker push wikibase/wikibase:1.30-bundle
+    docker push wikibase/wikibase:latest-bundle
 fi
