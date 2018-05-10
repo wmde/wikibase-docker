62ba24d94adf619ba120b4df2e7147c4aa548067
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-05-10 15:34:21 +0100

    Remove ENV vars at build time wdqs-frontend and enforce presence at runtime

--- wdqs-frontend/latest/entrypoint.sh
+++ wdqs-frontend/latest/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # This file is provided by the wikibase/wdqs-frontend docker image.
 
+# Test if required environment variables have been set
+REQUIRED_VARIABLES=(WIKIBASE_HOST WDQS_HOST)
+for i in ${REQUIRED_VARIABLES[@]}; do
+    eval THISSHOULDBESET=\$$i
+    if [ -z "$THISSHOULDBESET" ]; then
+    echo "$i is required but isn't set. You can pass it using either plain docker or docker-compose";
+    exit 1;
+    fi
+done
+
 set -eu
 
 export DOLLAR='$'
