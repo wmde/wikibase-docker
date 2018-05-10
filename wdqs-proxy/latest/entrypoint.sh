423916a05c1e71255f5fbe142166289f30d7636a
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-05-10 15:37:17 +0100

    enforce ENV presence at runtime in wdqs-proxy

--- wdqs-proxy/latest/entrypoint.sh
+++ wdqs-proxy/latest/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # This file is provided by the wikibase/wdqs-proxy docker image.
 
+# Test if required environment variables have been set
+REQUIRED_VARIABLES=(PROXY_PASS_HOST)
+for i in ${REQUIRED_VARIABLES[@]}; do
+    eval THISSHOULDBESET=\$$i
+    if [ -z "$THISSHOULDBESET" ]; then
+    echo "$i is required but isn't set. You can pass it using either plain docker or docker-compose";
+    exit 1;
+    fi
+done
+
 set -eu
 
 envsubst < /etc/nginx/conf.d/wdqs.template > /etc/nginx/conf.d/default.conf
