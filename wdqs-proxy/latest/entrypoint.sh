8935e15a784a8d8a2545276459f43d6e39ef0ce4
Thomas Arrow <tarrow@users.noreply.github.com>
2018-05-17 12:32:41 +0100

    Remove ENV vars at build time enforce presence at runtime (#21)
    * Remove ENV vars at build time wdqs-0.2.5 and enforce presence at runtime
    
    * Remove ENV vars at build time wdqs-0.3.0 and enforce presence at runtime
    
    * Remove ENV vars at build time wdqs-frontend and enforce presence at runtime
    
    * enforce ENV presence at runtime in wdqs-proxy
    
    * Remove ENV vars at build time wikibase-1.29 and enforce presence at runtime
    
    * Remove ENV vars at build time wikibase-1.30 and enforce presence at runtime
    
    * remove exposed port from wdqs DockerFile and place in compose
    
    * Add required environment variables into docker-compose.yml
    
    * Add required environment variables into docker-compose-build.yml
    
    * Make plain sh scripts POSIX compatible
    
    * Make missing variable message docker specific and clearer

--- wdqs-proxy/latest/entrypoint.sh
+++ wdqs-proxy/latest/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # This file is provided by the wikibase/wdqs-proxy docker image.
 
+# Test if required environment variables have been set
+if [ -z "$PROXY_PASS_HOST" ]; then
+echo "PROXY_PASS_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
+exit 1;
+fi
+
 set -eu
 
 envsubst < /etc/nginx/conf.d/wdqs.template > /etc/nginx/conf.d/default.conf
