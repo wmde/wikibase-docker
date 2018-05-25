505a6eb3d1241c9ab57dace53855e65eafd8a9ec
addshore <adamshorland@gmail.com>
2018-05-25 15:56:59 +0100

    Timeout of 120 for wait-for-it.sh calls

--- wikibase/1.30/base/entrypoint.sh
+++ wikibase/1.30/base/entrypoint.sh
@@ -14,11 +14,11 @@ done
 set -eu
 
 # Wait for the db to come up
-/wait-for-it.sh $DB_SERVER -t 60
+/wait-for-it.sh $DB_SERVER -t 120
 # Sometimes it appears to come up and then go back down meaning MW install fails
 # So wait for a second and double check!
 sleep 1
-/wait-for-it.sh $DB_SERVER -t 60
+/wait-for-it.sh $DB_SERVER -t 120
 
 # Do the mediawiki install (only if LocalSettings doesn't already exist)
 if [ ! -e "/var/www/html/LocalSettings.php" ]; then
