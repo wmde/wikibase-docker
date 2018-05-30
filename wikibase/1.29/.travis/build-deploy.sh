6b1a053c1dc163bc6ca30a78c83aecae30dcd089
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-05-30 14:58:32 +0100

    force pull and fix up versions

--- wikibase/1.29/.travis/build-deploy.sh
+++ wikibase/1.29/.travis/build-deploy.sh
@@ -2,7 +2,7 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../" -t wikibase/wikibase:1.29 -t wikibase/wikibase:legacy
+docker build --pull "$DIR/../" -t wikibase/wikibase:1.29 -t wikibase/wikibase:legacy
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
     docker push wikibase/wikibase:1.29
