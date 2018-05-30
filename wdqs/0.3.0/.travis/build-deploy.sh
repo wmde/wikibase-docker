8fe84b8a426a23d9c760cfc68821b0ee820e2225
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-05-30 13:44:37 +0100

    Use set -e to make the CI fail if the build fails

--- wdqs/0.3.0/.travis/build-deploy.sh
+++ wdqs/0.3.0/.travis/build-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e
 docker build "$DIR/../" -t wikibase/wdqs:0.3.0 -t wikibase/wdqs:latest
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
