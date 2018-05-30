c9486230d12ab9be70149cf428c3d51fdbbb0045
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-05-30 14:58:32 +0100

    force pull and fix up versions

--- wdqs-proxy/latest/.travis/build-deploy.sh
+++ wdqs-proxy/latest/.travis/build-deploy.sh
@@ -2,7 +2,7 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../" -t wikibase/wdqs-proxy:latest
+docker build --pull "$DIR/../" -t wikibase/wdqs-proxy:latest
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
     docker push wikibase/wdqs-proxy:latest
