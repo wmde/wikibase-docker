c6c2a0f7bda3af3a353fab38c802382267ff86a9
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-05-31 08:22:07 +0100

    Paramaterize namespace details

--- quickstatements/latest/entrypoint.sh
+++ quickstatements/latest/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Test if required environment variables have been set
-REQUIRED_VARIABLES=(OAUTH_CONSUMER_KEY OAUTH_CONSUMER_SECRET QS_PUBLIC_SCHEME_HOST_AND_PORT WB_PUBLIC_SCHEME_HOST_AND_PORT WIKIBASE_SCHEME_AND_HOST)
+REQUIRED_VARIABLES=(OAUTH_CONSUMER_KEY OAUTH_CONSUMER_SECRET QS_PUBLIC_SCHEME_HOST_AND_PORT WB_PUBLIC_SCHEME_HOST_AND_PORT WIKIBASE_SCHEME_AND_HOST WB_PROPERTY_NAMESPACE WB_PROPERTY_PREFIX WB_ITEM_NAMESPACE WB_ITEM_PREFIX)
 for i in ${REQUIRED_VARIABLES[@]}; do
     if ! [[ -v "$i" ]]; then
     echo "$i is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
