a865f0a9e4c1f7299243e935ba80bde117cd7433
Amir Sarabadani <Ladsgroup@gmail.com>
2018-12-19 13:59:11 +0100

    Remove --pull in docker build

--- wikibase/1.30/.travis/build-deploy.sh
+++ wikibase/1.30/.travis/build-deploy.sh
@@ -2,8 +2,8 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build --pull "$DIR/../base" -t wikibase/wikibase:1.30 -t wikibase/wikibase:1.30-base
-docker build --pull "$DIR/../bundle" -t wikibase/wikibase:1.30-bundle
+docker build "$DIR/../base" -t wikibase/wikibase:1.30 -t wikibase/wikibase:1.30-base
+docker build "$DIR/../bundle" -t wikibase/wikibase:1.30-bundle
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
     docker push wikibase/wikibase:1.30
