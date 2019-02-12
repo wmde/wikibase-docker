885607993acad732d59529d069d4b87d3484aec9
Amir Sarabadani <Ladsgroup@gmail.com>
2019-02-12 15:33:54 +0100

    Make mediawiki build a OAuth consumer for quickstatements and share it with quickstatements
    Bug: T205606

--- wikibase/1.31/bundle/extra-install.sh
+++ wikibase/1.31/bundle/extra-install.sh
@@ -3,3 +3,5 @@
 php /var/www/html/extensions/CirrusSearch/maintenance/updateSearchIndexConfig.php
 php /var/www/html/extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipParse
 php /var/www/html/extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipLinks --indexOnSkip
+
+php /var/www/html/extensions/OAuth/maintenance/createOAuthConsumer.php --approve --callbackUrl  $QS_PUBLIC_SCHEME_HOST_AND_PORT --callbackIsPrefix true --user Admin --name QuickStatements --description QuickStatements --version 1.0.1 --grants createeditmovepage editpage highvolume --jsonOnSuccess > /quickstatements/data/qs-oauth.json
--- wikibase/1.32/base/entrypoint.sh
+++ wikibase/1.32/base/entrypoint.sh
@@ -2,7 +2,7 @@
 # This file is provided by the wikibase/wikibase docker image.
 
 # Test if required environment variables have been set
-REQUIRED_VARIABLES=(MW_ADMIN_NAME MW_ADMIN_PASS MW_WG_SECRET_KEY DB_SERVER DB_USER DB_PASS DB_NAME)
+REQUIRED_VARIABLES=(MW_ADMIN_NAME MW_ADMIN_PASS MW_ADMIN_EMAIL MW_WG_SECRET_KEY DB_SERVER DB_USER DB_PASS DB_NAME)
 for i in ${REQUIRED_VARIABLES[@]}; do
     eval THISSHOULDBESET=\$$i
     if [ -z "$THISSHOULDBESET" ]; then
@@ -28,6 +28,7 @@ fi
 # Do the mediawiki install (only if LocalSettings doesn't already exist)
 if [ ! -e "/var/www/html/LocalSettings.php" ]; then
     php /var/www/html/maintenance/install.php --dbuser $DB_USER --dbpass $DB_PASS --dbname $DB_NAME --dbserver $DB_SERVER --lang $MW_SITE_LANG --pass $MW_ADMIN_PASS $MW_SITE_NAME $MW_ADMIN_NAME
+    php /var/www/html/maintenance/resetUserEmail.php --no-reset-password $MW_ADMIN_NAME $MW_ADMIN_EMAIL
 
     # Copy our LocalSettings into place after install from the template
     # https://stackoverflow.com/a/24964089/4746236
