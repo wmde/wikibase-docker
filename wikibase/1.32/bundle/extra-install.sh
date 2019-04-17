3c442c40ad4242400bdfb65dbf7b282b05df4c99
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-04-17 07:20:38 +0200

    Only generate OAuth data for QuickStatements if the file does not exist
    Somehow file gets overwritten with an empty file sometimes.
    This might, or might not, prevent this.

--- wikibase/1.32/bundle/extra-install.sh
+++ wikibase/1.32/bundle/extra-install.sh
@@ -4,6 +4,8 @@ php /var/www/html/extensions/CirrusSearch/maintenance/updateSearchIndexConfig.ph
 php /var/www/html/extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipParse
 php /var/www/html/extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipLinks --indexOnSkip
 
-php /var/www/html/extensions/OAuth/maintenance/createOAuthConsumer.php --approve --callbackUrl  $QS_PUBLIC_SCHEME_HOST_AND_PORT \
-	--callbackIsPrefix true --user Admin --name QuickStatements --description QuickStatements --version 1.0.1 \
-	--grants createeditmovepage --grants editpage --grants highvolume --jsonOnSuccess > /quickstatements/data/qs-oauth.json
+if [ ! -e "/quickstatements/data/qs-oauth.json" ]; then
+	php /var/www/html/extensions/OAuth/maintenance/createOAuthConsumer.php --approve --callbackUrl  $QS_PUBLIC_SCHEME_HOST_AND_PORT \
+		--callbackIsPrefix true --user Admin --name QuickStatements --description QuickStatements --version 1.0.1 \
+		--grants createeditmovepage --grants editpage --grants highvolume --jsonOnSuccess > /quickstatements/data/qs-oauth.json
+fi
