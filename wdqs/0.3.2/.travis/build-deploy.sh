2b120808502f7f810312bc6635286cefcacb80fb
Amir Sarabadani <Ladsgroup@gmail.com>
2019-07-09 18:02:44 +0200

    Create docker image for wdqs version 0.3.2
    Bug: T226939

--- wdqs/0.3.2/.travis/build-deploy.sh
+++ wdqs/0.3.2/.travis/build-deploy.sh
@@ -2,8 +2,9 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../" -t wikibase/wdqs:0.3.1
+docker build "$DIR/../" -t wikibase/wdqs:0.3.2 -t wikibase/wdqs:latest
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
-    docker push wikibase/wdqs:0.3.1
+    docker push wikibase/wdqs:0.3.2
+    docker push wikibase/wdqs:latest
 fi
---
+++ wdqs/0.3.2/runUpdate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# This file is provided by the wikibase/wdqs docker image.
+
+cd /wdqs
+
+# TODO env vars for entity namespaces, scheme and other settings
+/wait-for-it.sh $WIKIBASE_HOST:80 -t 120 -- \
+/wait-for-it.sh $WDQS_HOST:$WDQS_PORT -t 120 -- \
+./runUpdate.sh -h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseHost $WIKIBASE_HOST --wikibaseScheme $WIKIBASE_SCHEME --entityNamespaces $WDQS_ENTITY_NAMESPACES
