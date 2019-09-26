d1ceb34523cdd6797da5473e1709dc20e296ad1f
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-09-26 13:59:11 +0200

    Fixed username in the QuickStatement "token" generation

--- wikibase/1.31/bundle/extra-install.sh
+++ wikibase/1.31/bundle/extra-install.sh
@@ -5,5 +5,5 @@ php /var/www/html/extensions/CirrusSearch/maintenance/forceSearchIndex.php --ski
 php /var/www/html/extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipLinks --indexOnSkip
 
 php /var/www/html/extensions/OAuth/maintenance/createOAuthConsumer.php --approve --callbackUrl  $QS_PUBLIC_SCHEME_HOST_AND_PORT \
-	--callbackIsPrefix true --user Admin --name QuickStatements --description QuickStatements --version 1.0.1 \
+	--callbackIsPrefix true --user WikibaseAdmin --name QuickStatements --description QuickStatements --version 1.0.1 \
 	--grants createeditmovepage --grants editpage --grants highvolume --jsonOnSuccess > /quickstatements/data/qs-oauth.json
