49e0cf6c47d137ab25421ae22c8790d706c1f607
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2019-10-25 13:43:20 +0100

    Fix QS race condition
    Create qs ini in wikibase not qs.
    
    This should prevent the race
    condtion where making the OAuth consumer isn't possible due to
    the user not having been properly made yet.

--- quickstatements/latest/entrypoint.sh
+++ quickstatements/latest/entrypoint.sh
@@ -9,21 +9,11 @@ for i in ${REQUIRED_VARIABLES[@]}; do
     fi
 done
 
-OAUTH_REQUIRED_VARIABLES=(OAUTH_CONSUMER_KEY OAUTH_CONSUMER_SECRET)
-for i in ${REQUIRED_VARIABLES[@]}; do
-    if ! [[ -v "$i" || -f /quickstatements/data/qs-oauth.json ]]; then
-    echo "$i is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
-    exit 1;
-    fi
-done
-
-if [[ -f /quickstatements/data/qs-oauth.json ]]; then
-    export OAUTH_CONSUMER_KEY=$(jq -r '.key' /quickstatements/data/qs-oauth.json);
-    export OAUTH_CONSUMER_SECRET=$(jq -r '.secret' /quickstatements/data/qs-oauth.json);
+if [[ -v "$OAUTH_CONSUMER_KEY" && "$OAUTH_CONSUMER_SECRET" ]]; then
+    envsubst < /templates/oauth.ini > /quickstatements/data/oauth.ini;
 fi
 
 envsubst < /templates/config.json > /var/www/html/quickstatements/public_html/config.json
-envsubst < /templates/oauth.ini > /var/www/html/quickstatements/oauth.ini
 envsubst < /templates/php.ini > /usr/local/etc/php/conf.d/php.ini
 
 docker-php-entrypoint apache2-foreground
