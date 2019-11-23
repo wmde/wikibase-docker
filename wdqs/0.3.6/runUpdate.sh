c83b22035fd3448b289d598ea8e4c43b9bab6d7e
Mystou <1592048+Mystou@users.noreply.github.com>
2019-11-23 23:10:32 +0100

    Update the runUpdate.sh
    Replace deprecated --wikibaseHost and --wikibaseScheme options with --wikibaseUrl and --conceptUri
    https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual#Updater_options

--- wdqs/0.3.6/runUpdate.sh
+++ wdqs/0.3.6/runUpdate.sh
@@ -6,4 +6,4 @@ cd /wdqs
 # TODO env vars for entity namespaces, scheme and other settings
 /wait-for-it.sh $WIKIBASE_HOST:80 -t 300 -- \
 /wait-for-it.sh $WDQS_HOST:$WDQS_PORT -t 300 -- \
-./runUpdate.sh -h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseHost $WIKIBASE_HOST --wikibaseScheme $WIKIBASE_SCHEME --entityNamespaces $WDQS_ENTITY_NAMESPACES
+./runUpdate.sh -h http://$WDQS_HOST:$WDQS_PORT -- --wikibaseUrl $WIKIBASE_SCHEME://$WIKIBASE_HOST --conceptUri $WIKIBASE_SCHEME://$WIKIBASE_HOST --entityNamespaces $WDQS_ENTITY_NAMESPACES
