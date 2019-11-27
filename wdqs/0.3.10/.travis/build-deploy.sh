5285c94d763b715033ce37435756777b826b0c64
addshore <addshorewiki@gmail.com>
2019-11-27 12:19:58 +0000

    wdqs 0.3.10

--- wdqs/0.3.10/.travis/build-deploy.sh
+++ wdqs/0.3.10/.travis/build-deploy.sh
@@ -2,8 +2,9 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../" -t wikibase/wdqs:0.3.6 -t wikibase/wdqs:latest
+docker build "$DIR/../" -t wikibase/wdqs:0.3.10 -t wikibase/wdqs:latest
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
-    docker push wikibase/wdqs:0.3.6
+    docker push wikibase/wdqs:0.3.10
+    docker push wikibase/wdqs:latest
 fi
---
+++ wdqs/0.3.10/runBlazegraph.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This file is provided by the wikibase/wdqs docker image.
+
+cd /wdqs
+
+./runBlazegraph.sh
