2f43cd8dcbff139bb14ef221cb4d744a83c64f65
Dan Michael O. Heggø <danmichaelo@gmail.com>
2020-04-22 14:52:09 +0200

    Update Elasticsearch: 5.6.14 -> 6.5.4

---
+++ elasticsearch/6.5.4-extra/.travis/build-deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e
+docker build "$DIR/../" -t wikibase/elasticsearch:6.5.4-extra -t wikibase/elasticsearch:latest
+
+if [ "$SHOULD_DOCKER_PUSH" = true ]; then
+    docker push wikibase/elasticsearch:6.5.4-extra
+    docker push wikibase/elasticsearch:latest
+fi
---
+++ elasticsearch/6.5.4-extra/Dockerfile
@@ -0,0 +1,3 @@
+FROM elasticsearch:6.5.4
+RUN ./bin/elasticsearch-plugin install org.wikimedia.search:extra:6.5.4
+RUN ./bin/elasticsearch-plugin install org.wikimedia.search.highlighter:experimental-highlighter-elasticsearch-plugin:6.5.4
