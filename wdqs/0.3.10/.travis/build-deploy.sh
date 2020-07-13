206039f619e40693b4df06cdc4d82fadf87cbbd5
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-07-13 12:38:51 +0200

    Remove 0.3.10 as latest build.

--- wdqs/0.3.10/.travis/build-deploy.sh
+++ wdqs/0.3.10/.travis/build-deploy.sh
@@ -2,9 +2,8 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../" -t wikibase/wdqs:0.3.10 -t wikibase/wdqs:latest
+docker build "$DIR/../" -t wikibase/wdqs:0.3.10
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
     docker push wikibase/wdqs:0.3.10
-    docker push wikibase/wdqs:latest
 fi
