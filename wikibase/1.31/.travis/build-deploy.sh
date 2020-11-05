faf7ebb9b7e103786d519de2a7e11309d25a7922
rosalie <perside.rosalie@wikimedia.de>
2020-11-05 14:13:53 +0100

    Add 1.35 to travis build and tag 1.35 as latest in README

--- wikibase/1.31/.travis/build-deploy.sh
+++ wikibase/1.31/.travis/build-deploy.sh
@@ -2,14 +2,11 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../base" -t wikibase/wikibase:1.31 -t wikibase/wikibase:1.31-base -t wikibase/wikibase:lts -t wikibase/wikibase:lts-base
-docker build "$DIR/../bundle" -t wikibase/wikibase:1.31-bundle -t wikibase/wikibase:lts-bundle
+docker build "$DIR/../base" -t wikibase/wikibase:1.31 -t wikibase/wikibase:1.31-base
+docker build "$DIR/../bundle" -t wikibase/wikibase:1.31-bundle
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
     docker push wikibase/wikibase:1.31
-    docker push wikibase/wikibase:lts
     docker push wikibase/wikibase:1.31-base
-    docker push wikibase/wikibase:lts-base
     docker push wikibase/wikibase:1.31-bundle
-    docker push wikibase/wikibase:lts-bundle
 fi
